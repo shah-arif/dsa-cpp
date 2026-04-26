81f93260ee38eee3164f2f7a6bebd50b600d68cf
Shah Arif <shaharifabdullah@gmail.com>
2026-04-27 00:37:13 +0600

    array product calcualtion

--- algorithm/array_calculation.cpp
+++ algorithm/array_calculation.cpp
@@ -9,11 +9,21 @@ int sumOfArray(int array[], int size){
     return sum;
 }
 
+int productOfArray(int array[], int size){
+    int product = 1;
+    for (int i = 0; i < size; i++){
+        product = product * array[i];
+    }
+    return product;
+}
+
 int main(){
-    int array[] = {1, 2, 3, 4};
+    int array[] = {1, 2, 3, 4, 4};
     int size = sizeof(array) / sizeof(array[0]);
-    int res = sumOfArray(array, size);
-    cout << res << endl;
+    int sum = sumOfArray(array, size);
+    int product = productOfArray(array, size);
+    cout << "Sum: " << sum << endl;
+    cout << "Product: " << product << endl;
 
 
     return 0;
