ea7303e8f7ae040c8f6c0affaae5cbf0ee1d33ed
Shah Arif <shaharifabdullah@gmail.com>
2026-04-27 23:41:23 +0600

    unique value

---
+++ algorithm/unique_value.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+
+bool isUnique(int array[], int size) {
+    for (int i = 0; i < size; i++){
+        for (int j = i+1; j < size; j++){
+            if (array[j] == array[i]){
+                return false;
+            }
+        }
+
+    }
+    return true;
+}
+
+int main(){
+    int array[] = {1, 2, 4, 3, 7, 6};
+    int size = sizeof(array) / sizeof(array[0]);
+    bool res = isUnique(array, size);
+    cout << res << endl;
+    
+    return 0;
+}
